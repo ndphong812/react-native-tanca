5645897d85d57553a75706cd634170b0f94e66cb
ndphong812 <ndphong812@gmail.com>
2023-06-07 14:01:52 +0700

    add assets, create intro page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
+import {StatusBar, useColorScheme} from 'react-native';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store';
 import {NavigationContainer} from '@react-navigation/native';
@@ -12,24 +7,27 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import IntroPage from './src/pages/intro';
 import LoginPage from './src/pages/login';
 import RegisterPage from './src/pages/register';
+import {NativeBaseProvider} from 'native-base';
+import {theme} from 'utils/css';
 
 const Stack = createNativeStackNavigator();
-
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <Provider store={store}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Intro"
-          screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Intro" component={IntroPage} />
-          <Stack.Screen name="Login" component={LoginPage} />
-          <Stack.Screen name="Register" component={RegisterPage} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <NativeBaseProvider theme={theme}>
+      <Provider store={store}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Intro"
+            screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Intro" component={IntroPage} />
+            <Stack.Screen name="Login" component={LoginPage} />
+            <Stack.Screen name="Register" component={RegisterPage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </NativeBaseProvider>
   );
 }
 
